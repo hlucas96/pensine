58d095f4a53594911dda44dc7909a544c1e1db4b
hlucas96 <lucashugo96@hotmail.fr>
2018-07-20 18:46:04 +0200

    Redirection after rating

--- app/controllers/rating_controller.rb
+++ app/controllers/rating_controller.rb
@@ -1,6 +1,6 @@
 class RatingController < ApplicationController
   def vote
     Rating.create(quote_id: params['quote_id'], value: params['value'])
-    redirect_to quote_path(params['quote_id'])
+    redirect_back(fallback_location: root_path)
   end
 end
